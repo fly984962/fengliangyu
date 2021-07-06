31da8026fd6b97f7c6b59bccab5570d70039209c
冯椋昱(w_fengliangyu) <984962790@qq.com>
2021-07-06 22:27:30 +0800

    merage

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "http://82.156.195.54/",
         pathRewrite: {
           "^/api":""
         },
